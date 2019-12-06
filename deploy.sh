9ad3c8e88170e267143f48b0e2834a27d34c7b84
vimal-kanagaraj <vimal.kanagaraj@gmail.com>
2019-12-06 08:31:46 +0530

    Corrected docker push command for multiclient

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 docker build -t vimalkanagaraj/multi-client:latest -t vimalkanagaraj/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t vimalkanagaraj/multi-client:latest -t vimalkanagaraj/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t vimalkanagaraj/multi-server:latest -t vimalkanagaraj/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t vimalkanagaraj/multi-worker:latest -t vimalkanagaraj/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push vimalkanagaraj/multi=client:latest
+docker push vimalkanagaraj/multi-client:latest
 docker push vimalkanagaraj/multi-server:latest
 docker push vimalkanagaraj/multi-worker:latest
 
-docker push vimalkanagaraj/multi=client:$GIT_SHA
+docker push vimalkanagaraj/multi-client:$GIT_SHA
 docker push vimalkanagaraj/multi-server:$GIT_SHA
 docker push vimalkanagaraj/multi-worker:$GIT_SHA
 
